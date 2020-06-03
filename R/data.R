6dcfa6d853692bea6d771ae41754476eac12b589
Marta Karas <martakaras@MacBook-Pro-9.local>
2020-06-02 22:58:18 -0400

    add data and data documentation

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' Names of exemplary accelerometry data file.
+#'
+#' Names of exemplary accelerometry data files attached to the package.
+#' These data can be accessed via
+#' \code{system.file("extdata", "data_file_name.csv", package = "arctools")}.
+#'
+#' @format A character vector.
+"extdata_fnames"
