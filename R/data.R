52e12b3806b04038a3c2ab4bd216b665b1f10d73
Marta Karas <martakaras@MacBook-Pro-9.local>
2020-06-02 23:14:24 -0400

    add get_actigraph_SN()

--- R/data.R
+++ R/data.R
@@ -2,7 +2,9 @@
 #'
 #' Names of exemplary accelerometry data files attached to the package.
 #' These data can be accessed via
-#' \code{system.file("extdata", "data_file_name.csv", package = "arctools")}.
+#' \code{system.file("extdata", "<data_file_name>.csv", package = "arctools")}.
+#'
+#' @docType data
 #'
 #' @format A character vector.
 "extdata_fnames"
